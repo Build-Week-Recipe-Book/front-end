4abb67d9d9b20639b2671a8c005fb74ab03540bb
mrn720 <matt.ryan.norris@gmail.com>
2021-07-22 18:20:41 -0500

    Made a user homepage for users to be directed to upon login or user creation

---
+++ src/Components/UserHome.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link, useRouteMatch } from 'react-router-dom'
+import styled from 'styled-components'
+import photo from './FoodPhoto2.jpg'
+ 
+function UserHome() {
+
+    const { url } = useRouteMatch()
+
+    return (
+        <HomeDiv>
+            <h1>Welcome to Secret Family Recipes!</h1>
+            <br/>
+            <MainImg src = {photo} alt = 'Food Photo' />
+            <br/>
+            <ButtonDiv>
+                <Link to = {`${url}create-recipe`}><Button id = "create-recipe">Create Recipe</Button></Link>
+                <Link to = {`${url}view-recipes`}><Button id = "view-recipes">View Recipes</Button></Link>
+            </ButtonDiv>
+        </HomeDiv>
+    )
+}
+
+const HomeDiv = styled.div`
+display: flex;
+justify-content: start;
+align-items: center;
+flex-direction: column;
+width: 100%;
+height: 100vh;
+`
+
+const MainImg = styled.img`
+height: 65vh;
+`
+
+const ButtonDiv = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+width: 20%;
+`
+
+const Button = styled.button`
+background: maroon;
+color: white;
+height: 30px;
+border-radius: 5px;
+`
+
+export default UserHome
