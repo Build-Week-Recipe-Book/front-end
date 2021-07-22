5496ab951a6d5e4c7be33a2fed95f10caf7cde81
Mallory <mallorydoescoding@gmail.com>
2021-07-22 16:42:28 -0500

    Form state added, values added to each input, and change handler created.

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,28 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+
+    //States
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        username: '',
+        password: '',
+        terms: false,
+    })
+
+    //Change handler
+    const change = event => {
+        const { checked, value, name, type } = event.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        setForm({ ...form, [name]: valueToUse })
+    }
+
     return (
         <div className='formContainer'>
             <form>
+                
                 <label>Name:
-                    <input type='text' name='name' />
+                    <input type='text' name='name' value={form.name} onChange={change} />
                 </label>
+
                 <br />
+
                 <label>Email:
-                    <input type='text' name='email' />
+                    <input type='text' name='email' value={form.email} onChange={change} />
                 </label>
+
                 <br />
+
                 <label>Username:
-                    <input type='text' name='username' />
+                    <input type='text' name='username' value={form.username} onChange={change} />
                 </label>
+
                 <br />
+
                 <label>Password:
-                    <input type='text' name='password' />
+                    <input type='text' name='password' value={form.password} onChange={change} />
                 </label>
+
                 <br />
+
                 <label>Terms of Service:
-                    <input type='checkbox' name='terms' />
+                    <input type='checkbox' name='terms' checked={form.terms} onChange={change} />
                 </label>
+
+                <br />
+
+                <button>Submit</button>
+
             </form>
         </div>
     )
