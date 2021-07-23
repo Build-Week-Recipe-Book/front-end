8dc24e22a4eb3d17612ce6158d99d15db1ce6e10
Mallory <mallorydoescoding@gmail.com>
2021-07-22 19:50:47 -0500

    Form state added, values added to inputs, and change handler created. Also add a checkbox for remember my info to the form.

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    //States
+    const [form, setForm] = useState({
+        username: '',
+        password: '',
+    })
+
+    //Change Handler
+    const change = event => {
+        const { name, type, value, checked } = event.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        setForm({ ...form, [name]: valueToUse })
+    }
+
     return (
         <div>
             
             <form>
 
                 <label>Username:
-                    <input type='text' name='username' />
+                    <input type='text' name='username' value={form.username} onChange={change} />
                 </label>
 
                 <br />
 
                 <label>Password:
-                    <input type='text' name='password' />
+                    <input type='text' name='password' value={form.password} onChange={change} />
                 </label>
 
                 <br />
 
+                <label>Remember my info:
+                    <input type='checkbox' name='remember' checked={form.remember} onChange={change} />
+                </label>
+
                 <button>Login</button>
+
             </form>
         </div>
     )
