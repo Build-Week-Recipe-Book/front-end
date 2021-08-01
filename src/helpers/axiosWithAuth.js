b74abc2ae49e12d84ccd4ab4f39adb1cc5b0b0ce
SavvyPlusPlus <info@savvydc.com>
2021-08-01 19:58:42 -0400

    Done

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -2,7 +2,7 @@ import { axiosWithAuth } from "./axiosWithAuth";
 
 export const fetchFoods = () => {
     return axiosWithAuth()
-    .get("/...")
+    .get("/https://reqres.in/api/users?page=2")
     .then((res) => res)
     .catch((err) =>err);
 };
